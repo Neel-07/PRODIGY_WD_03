6384fc3c48306f26c13bab1068745b36d77c8499
Neel Mishra <neelmishra9125@gmail.com>
2024-01-16 09:27:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,20 +37,22 @@ function handleCellClick(e) {
 }
 
 function checkWin() {
-    const winningPlayer = board[0][0] || board[1][1] || board[2][2];
     for (let i = 0; i < 3; i++) {
-        if ((board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) ||
-            (board[0][i] && board[0][i] === board[1][i] && board[0][i] === board[2][i])) {
-            return board[i][0] || board[0][i];
+        if (board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+            return board[i][0];
+        }
+        if (board[0][i] !== 0 && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+            return board[0][i];
         }
     }
-    if (winningPlayer && winningPlayer === board[1][1] && 
-        ((winningPlayer === board[0][0] && winningPlayer === board[2][2]) ||
-        (winningPlayer === board[0][2] && winningPlayer === board[2][0]))) {
-        return winningPlayer;
+    if (board[1][1] !== 0 && 
+        ((board[1][1] === board[0][0] && board[1][1] === board[2][2]) ||
+        (board[1][1] === board[0][2] && board[1][1] === board[2][0]))) {
+        return board[1][1];
     }
     return false;
 }
+
 
 function resetGame() {
     board.forEach((row) => row.fill(''));
